b04870eb3a9e639adcff4e41d1662d89835de4bf
kohei <bhkbb0551@gmail.com>
2024-01-06 19:27:42 +0900

    ゼロダウンタイムデプロイの実装したバージョンのモジュールを使用

--- prod/servers/main.tf
+++ prod/servers/main.tf
@@ -1,10 +1,13 @@
 module "servers" {
-  source = "git::https://github.com/Kohei-Suzuki22/detailed-explanation-terraform3-modules.git//servers?ref=v1.1.0"
+  source = "git::https://github.com/Kohei-Suzuki22/detailed-explanation-terraform3-modules.git//servers?ref=v1.2.0"
+  # source = "../../../modules/servers"
 
   cluster_name = "prod"
   remote_state_bucket = "detailed-explanation-terraform"
   globals_remote_state_key = "globals/terraform.tfstate"
   databases_remote_state_key = "prod/databases/terraform.tfstate"
+  ami = "ami-07c589821f2b353aa"
+  server_text = "Server text"
   server_port = var.server_port
   instance_type = "t2.micro"
   autoscaling_min_size = 2
