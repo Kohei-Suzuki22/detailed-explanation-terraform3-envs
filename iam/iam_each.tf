f48cda00d6244d6d6219208f54dc6f25cb0bea3a
kohei <bhkbb0551@gmail.com>
2024-01-06 18:38:52 +0900

    if文字列ディレクティブを使った条件分岐

--- iam/iam_each.tf
+++ iam/iam_each.tf
@@ -37,13 +37,69 @@ output "upper_roles" {
 }
 
 
-# ↓　output -> "neo, trinity, morpheus, "
 # 表示する内容は、"%{for .... } <BODY> %{endfor}" の <BODY>の部分。
 output "for_directive" {
   value = "%{for name in var.names}${name}, %{endfor}"
 }
+# output -> "neo, trinity, morpheus, "
 
 output "for_directive_index" {
   value = "%{for i, name in var.names}(${i}): ${name}, %{endfor}"
-  
 }
+
+# "%{if ....} <TRUEVAL> %{endif}"  :  if ... が trueの時だけ表示する。
+
+output "for_directive_index_if" {
+  value = <<EOF
+    %{for i, name in var.names}(${i}): ${name}
+      %{if i < length(var.names) - 1}, %{endif}
+    %{endfor}
+  EOF
+}
+
+// output -> ヒアドキュメントを <<EOF EOFで使うと、内部の改行・空白などあらゆるスペースが文字列として認識されてしまう。
+# <<EOT
+#     (0): neo
+#       , 
+#     (1): trinity
+#       , 
+#     (2): morpheus
+#      
+#    
+#
+# EOT
+//
+
+output "for_directive_index_if_no_space" {
+  value = <<EOF
+  %{~ for i, name in var.names ~}(${i}):${name}
+    %{~ if i < length(var.names) - 1 ~}, %{~ endif ~}
+  %{~ endfor ~}
+  EOF
+}
+
+// output -> チルダ「~」を文字列ディレクティブに追加することで、~の前後のスペースが削除されるらしい。(うまくいってないかも???)
+# <<EOT
+# (0):neo
+# ,(1):trinity
+# ,(2):morpheus
+#
+# EOT
+//
+
+output "for_directive_index_if_else_no_space" {
+  value = <<EOF
+  %{~ for i, name in var.names ~}(${i}):${name}
+    %{~ if i < length(var.names) - 1 ~}, %{else}.%{~ endif ~}
+  %{~ endfor ~}
+  EOF
+}
+
+// output -> if elseを使える。
+# <<EOT
+# (0):neo
+# , (1):trinity
+# , (2):morpheus
+# .
+# EOT
+//
